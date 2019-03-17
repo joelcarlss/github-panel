68654c2e5be37898344e0cc5c5a484282f2d8e51
joel <zorcey@gmail.com>
2019-03-17 14:00:22 +0100

    Adding random data to chart.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Chart from './Chart'
 
 const Statistics = (props) => {
   return (
     <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
       <p>{props.match.params.id}</p>
+      <Chart />
     </div>
   )
 }
