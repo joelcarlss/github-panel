a0122debbf345195e504608ff482f737a0b42ca7
joel <zorcey@gmail.com>
2019-03-17 13:28:16 +0100

    Installing react-chartjs-2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import {useAppState} from '../pages/useAppState'
 
 import { withStyles } from '@material-ui/core/styles'
