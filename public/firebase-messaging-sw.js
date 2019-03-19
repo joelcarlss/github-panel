498d67abab231b4a577d4191212b04168e0ebb47
joel <zorcey@gmail.com>
2019-03-19 18:11:51 +0100

    Removing unneeded data in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,11 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 // Initialize Firebase
 
 var config = {
-  apiKey: 'AIzaSyBtaTMfjwwCtyuqvxmbMdEPYQfYKyTbUQc',
-  authDomain: 'githubdashboard-1cbfc.firebaseapp.com',
-  databaseURL: 'https://githubdashboard-1cbfc.firebaseio.com',
-  projectId: 'githubdashboard-1cbfc',
-  storageBucket: 'githubdashboard-1cbfc.appspot.com',
   messagingSenderId: '1073613111252'
 }
 
