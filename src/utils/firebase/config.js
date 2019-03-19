df4cd4c10b0495c6ede257059739662ddac37a30
joel <zorcey@gmail.com>
2019-03-19 18:18:35 +0100

    Creating environmental variables

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -3,12 +3,12 @@ import * as firebase from 'firebase'
 // Initialize Firebase
 
 var config = {
-  apiKey: 'AIzaSyBtaTMfjwwCtyuqvxmbMdEPYQfYKyTbUQc',
-  authDomain: 'githubdashboard-1cbfc.firebaseapp.com',
-  databaseURL: 'https://githubdashboard-1cbfc.firebaseio.com',
-  projectId: 'githubdashboard-1cbfc',
-  storageBucket: 'githubdashboard-1cbfc.appspot.com',
-  messagingSenderId: '1073613111252'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 
